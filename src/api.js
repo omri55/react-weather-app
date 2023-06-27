96583972387706eb6a11aed97d971ca0372257e2
omri55 <49839633+omri55@users.noreply.github.com>
2023-06-27 17:09:11 +0300

    upload project to git

---
+++ src/api.js
@@ -0,0 +1,15 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'f80b3e545bmsh0c52c78094ec93dp18108cjsn5d0d9550bc9e',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
+export const WEATHER_API_KEY = '04025068e6cb1f3490f7f8f3064d4338'
+
+
+
+
